f764036631f4923b31983a8fb3def5c9835368b5
phillipstemmlar <phillipstemmlar@gmail.com>
2020-05-30 14:00:55 +0200

    chore: Configured the server's package.json to automatically build the react-app before starting

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { PORT } = process.env;
 const finalhandler = require('finalhandler');
 const serveStatic = require('serve-static');
 
-const serve = serveStatic('./data-visualisation-app/build/static/');
+const serve = serveStatic('./data-visualisation-app/build/');
 
 var server = http.createServer(function (req, res) {
   var done = finalhandler(req, res);
