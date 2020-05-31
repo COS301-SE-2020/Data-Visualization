f9abf20403503c2b82b6e6adc2e611bb9af86e84
Byron Tomkinson <48924346+byrongt12@users.noreply.github.com>
2020-05-31 19:09:07 +0200

    Added files for starting the integration
    Added files for starting the integration from the UI/UX model designed in Adobe XD

---
+++ XD export/WebComponent.stories.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Web from "./WebComponent";
+
+export const myProps = {
+  id: "1",
+  className: "mySampleClass",
+  sampleString: "Test Task",
+  sampleDate: new Date(2018, 0, 1, 9, 0)
+};
+
+export const actions = {
+  onActionOne: action("onActionOne"),
+  onActionTwo: action("onActionTwo")
+};
+
+storiesOf("Web", module)
+  .add("default", () => <Web {...myProps} {...actions} />)
+  .add("pinned", () => <Web {...myProps} pinned={true} {...actions} />)
+  .add("archived", () => <Web {...myProps} archived={true} {...actions} />);
