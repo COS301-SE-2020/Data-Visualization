1fd3383535ae8e2ec3a43b77cc1dc26c86c7578c
Marco <U18026975@tuks.co.za>
2020-06-09 16:17:57 +0200

    Changed EditDashboard into a function

--- data-visualisation-app/src/components/EditDashboard/EditDashboard.js
+++ data-visualisation-app/src/components/EditDashboard/EditDashboard.js
@@ -1,25 +1,20 @@
 import React from 'react';
 
-class EditDashboard extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            //add: props.AddConnection,
-            //filter: props.Filter,
-            delete: props.Delete,
-        }
-    }
+function EditDashboard(props) {
+    this.state = {
+        //add: props.AddConnection,
+        //filter: props.Filter,
+        delete: props.Delete,
+    };
 
-    render() {
-        return (
-            <div className="EditDashboard">
-                <button type="Submit" onClick={() => alert("Functionality not ready yet")}>Add Connection</button>
-                <button type="Submit" onClick={() => alert("Functionality not ready yet")}>Filter</button>
-                <Suggestions />
-                <button type="Submit" style="float: right" onClick={() => this.state.delete}>Delete Dashboard</button>
-            </div>
-        );
-    }
+    return (
+        <div className="EditDashboard">
+            <button type="Submit" onClick={() => alert("Functionality not ready yet")}>Add Connection</button>
+            <button type="Submit" onClick={() => alert("Functionality not ready yet")}>Filter</button>
+            <Suggestions/>
+            <button type="Submit" style="float: right" onClick={() => this.state.delete}>Delete Dashboard</button>
+        </div>
+    );
 }
 
 export default EditDashboard;
