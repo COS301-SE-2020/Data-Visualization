5c94a49a6fb2ad9f07f722a36e1078f5d061fa4c
elnapistorius <elnapistorius1999@gmail.com>
2020-07-01 16:46:13 +0200

    chore: added user server-side validation

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.use(session({
 
 router.post('/user-login', (req, res) => {
     let userName = checkName(req.body.name);
-    let userPassword = checkUserPassword(req.body.password);
+    let userPassword = checkUserPasswordLogin(req.body.password);
     Rest.login(
         userName,
         userPassword,
@@ -43,7 +43,7 @@ router.post('/user-register', (req, res) => {
     let userName = checkName(req.body.name);
     let userSurname = checkName(req.body.surname);
     let userEmail = checkUserEmail(req.body.email);
-    let userPassword = checkUserPassword(req.body.password);
+    let userPassword = checkUserPasswordRegister(req.body.password, req.body.password,req.body.name );
 
     Rest.register(
         userName,
@@ -64,14 +64,70 @@ function error(res, err) {
     console.error(err);
     res.status(400).json({ message: 'Error Occurred' });
 }
-function checkName(userName){
-    // Do user name validation
+function checkName(name){
+    let re = /^\w+$/;
+    let valid = true;
+    if (name.trim().length === 0) {
+        // name can not be blank
+        valid = false;
+    } else if (!re.test(name)) {
+        valid = false;
+    }
+    return valid
 }
-function checkUserEmail(userEmail){
-    // Do user email validation
+function checkUserEmail(email){
+    let emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    let valid = true;
+    if (email.trim().length === 0) {
+        // email required
+        valid = false;
+    } else if (!emailReg.test(email)) {
+        //email does not exist
+        valid = false;
+    }
+    return valid;
 }
-function checkUserPassword(userPassword){
+function checkUserPasswordLogin(userPassword){
     // Do user password validation
 }
-
+function checkUserPasswordRegister(password, confirmPassword, name){
+    let valid = true;
+    if (password.value.trim().length === 0) {
+        // password cannot be empty
+        valid = false;
+    } else if (password !== "" && password === confirmPassword) {
+        if (password.length < 8) {
+            //password must be 8 letters
+            valid = false;
+        }
+        if (password === name) {
+            //Password must be different than name
+            valid = false;
+        }
+        re = /[0-9]/;
+        if (!re.test(password)) {
+            valid = false;
+            // password password must contain at least one number (0-9)
+        }
+        re = /[a-z]/;
+        if (!re.test(password)) {
+            valid = false;
+           // password must contain at least one lowercase letter (a-z)!
+        }
+        re = /[A-Z]/;
+        if (!re.test(password)) {
+            valid = false;
+            // password must contain at least one capital letter
+        }
+        re = /[!@#$%^&*]/;
+        if (!re.test(password)) {
+            valid = false;
+          // password must contain at least one special character
+        }
+    } else {
+        valid = false;
+        //confirm password
+    }
+    return valid;
+}
 module.exports = router;
