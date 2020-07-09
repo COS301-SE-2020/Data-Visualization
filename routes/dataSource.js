11a1c4d3c0aea989c39fbc1a584fb68afd10e9cd
elnapistorius <elnapistorius1999@gmail.com>
2020-07-09 14:43:13 +0200

    chore: added validation for roots

--- routes/dataSource.js
+++ routes/dataSource.js
@@ -5,34 +5,55 @@ const router = express.Router();
 const { Rest } = require('../controllers');
 
 router.post('/list', (req, res) => {
-  Rest.getDataSourceList(
-    req.body.email,
-    (list) => res.status(200).json(list),
-    (err) => error(res, err)
-  );
+  if(Object.keys(req.body).length === 0){
+    error(res, { error: 'Body Undefined' }, 400);
+  }
+  else {
+    Rest.getDataSourceList(
+        req.body.email,
+        (list) => res.status(200).json(list),
+        (err) => error(res, err)
+    );
+  }
 });
 
 router.post('/add', (req, res) => {
-  Rest.addDataSource(
-    req.body.email,
-    req.body.dataSourceUrl,
-    () => res.status(200).json({ message: 'Successfully Added To Data Source' }),
-    (err) => error(res, err)
-  );
+  if(Object.keys(req.body).length === 0){
+    error(res, { error: 'Body Undefined' }, 400);
+  }
+  else if(req.body.dataSourceUrl === undefined){
+    error(res, { error: 'Data Source url Undefined' }, 400);
+  }
+  else {
+    Rest.addDataSource(
+        req.body.email,
+        req.body.dataSourceUrl,
+        () => res.status(200).json({message: 'Successfully Added To Data Source'}),
+        (err) => error(res, err)
+    );
+  }
 });
 
 router.post('/remove', (req, res) => {
-  Rest.removeDataSource(
-    req.body.email,
-    req.body.dataSourceID,
-    () => res.status(200).json({ message: 'Successfully Removed Data Source' }),
-    (err) => error(res, err)
-  );
+  if(Object.keys(req.body).length === 0){
+    error(res, { error: 'Body Undefined' }, 400);
+  }
+  else if(req.body.dataSourceID === undefined){
+    error(res, { error: 'Data Source Id Undefined' }, 400);
+  }
+  else {
+    Rest.removeDataSource(
+        req.body.email,
+        req.body.dataSourceID,
+        () => res.status(200).json({message: 'Successfully Removed Data Source'}),
+        (err) => error(res, err)
+    );
+  }
 });
 
-function error(res, err) {
+function error(res, err, status= 404) {
   console.error(err);
-  res.status(400).json({ message: 'Error Occurred' });
+  res.status(status).json(err);
 }
 
 module.exports = router;
--- routes/graphs.js
+++ routes/graphs.js
@@ -6,49 +6,90 @@ const { Rest } = require('../controllers');
 
 //  1. GET_GRAPHS (THIS WILL RETURN JUST DASHBOARDS WITH THEIR NAME, DESCRIPTION AND COLOUR)
 router.post('/list', (req, res) => {
-  Rest.getGraphList(
-    req.body.dashboardID,
-    (list) => res.status(200).json(list),
-    (err) => error(res, err)
-  );
+  if(Object.keys(req.body).length === 0){
+    error(res, { error: 'Body Undefined' }, 400);
+  }
+  else if(req.body.dashboardID === undefined){
+    error(res, { error: 'Dashboard Id Undefined' }, 400);
+  }
+  else {
+    Rest.getGraphList(
+        req.body.dashboardID,
+        (list) => res.status(200).json(list),
+        (err) => error(res, err,404)
+    );
+  }
 });
 
 //  2. UPDATE_GRAPH
 //  => PUT(graphType, graphID)
 router.post('/update', (req, res) => {
-  Rest.updateGraph(
-    req.body.graphID,
-    req.body.fields,
-    req.body.data,
-    () => res.status(200).json({ message: 'Successfully Updated Graph' }),
-    (err) => error(res, err)
-  );
+  if(Object.keys(req.body).length === 0){
+    error(res, { error: 'Body Undefined' }, 400);
+  }
+  else if(req.body.graphID === undefined){
+    error(res, { error: 'Graph Id Undefined' }, 400);
+  }
+  else if(req.body.fields === undefined){
+    error(res, { error: 'Fields Undefined' }, 400);
+  }
+  else if( req.body.data === undefined){
+    error(res, { error: 'Data Undefined' }, 400);
+  }
+  else {
+    Rest.updateGraph(
+        req.body.graphID,
+        req.body.fields,
+        req.body.data,
+        () => res.status(200).json({message: 'Successfully Updated Graph'}),
+        (err) => error(res, err,404)
+    );
+  }
 });
 
 //  3. ADD_GRAPH_TO_DASHBOARD
 // => POST (dashboardID, graphID)
 router.post('/add', (req, res) => {
-  Rest.addGraph(
-    req.body.graphTypeID,
-    req.body.dashboardID,
-    () => res.status(200).json({ message: 'Successfully Added To Dashboard' }),
-    (err) => error(res, err)
-  );
+  if(Object.keys(req.body).length === 0){
+    error(res, { error: 'Body Undefined' }, 400);
+  }
+  else if(req.body.graphTypeID === undefined){
+    error(res, { error: 'Graph Type Id Undefined' }, 400);
+  }
+  else if(req.body.dashboardID === undefined){
+    error(res, { error: 'Dashboard Id Undefined' }, 400);
+  }
+  else {
+    Rest.addGraph(
+        req.body.graphTypeID,
+        req.body.dashboardID,
+        () => res.status(200).json({message: 'Successfully Added To Dashboard'}),
+        (err) => error(res, err,404)
+    );
+  }
 });
 
 //  4. DELETE_GRAPH_FROM_DASHBOARD
 //  => DELETE (graphID)
 router.post('/remove', (req, res) => {
-  Rest.removeGraph(
-    req.body.graphID,
-    () => res.status(200).json({ message: 'Successfully Removed Graph' }),
-    (err) => error(res, err)
-  );
+  if(Object.keys(req.body).length === 0){
+    error(res, { error: 'Body Undefined' }, 400);
+  }
+  else if(req.body.graphID === undefined){
+    error(res, { error: 'Graph Id Undefined' }, 400);
+  }
+  else {
+    Rest.removeGraph(
+        req.body.graphID,
+        () => res.status(200).json({message: 'Successfully Removed Graph'}),
+        (err) => error(res, err,404)
+    );
+  }
 });
 
-function error(res, err) {
+function error(res, err, status= 404) {
   console.error(err);
-  res.status(400).json({ message: 'Error Occurred' });
+  res.status(status).json(err);
 }
 
 module.exports = router;
