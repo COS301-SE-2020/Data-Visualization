f6f6894695eb19630831247bf371ea835e045049
elnapistorius <elnapistorius1999@gmail.com>
2020-07-09 09:58:44 +0200

    chore: added apikey generator used for validation

--- controllers/database/databaseController.js
+++ controllers/database/databaseController.js
@@ -217,5 +217,13 @@ function fieldUpdates(fields, data) {
   }
   return output;
 }
-
+function generateApiKey() {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < 20; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 module.exports = Database;
