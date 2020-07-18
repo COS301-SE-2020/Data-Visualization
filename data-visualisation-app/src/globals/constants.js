7a2dd556da8f2772fb5dc87184842ff2a323e7b4
Gian Uys <gianuys@gmail.com>
2020-07-18 14:24:18 +0200

    Changed production mode constant

--- data-visualisation-app/src/globals/constants.js
+++ data-visualisation-app/src/globals/constants.js
@@ -47,7 +47,8 @@ export const LOADER = <Spin/>;
 /**
  *   Server Constants
  */
-export const PRODUCTION_MODE = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? true : false;
+// export const PRODUCTION_MODE = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? true : false;
+export const PRODUCTION_MODE = true;
 export const URL_HOST = {
     PRODUCTION: 'https://data-visualisation-prod.herokuapp.com',
     DEVELOPMENT: 'https://data-visualisation-dev.herokuapp.com',
