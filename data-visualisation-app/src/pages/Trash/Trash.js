d9199362bda86602fbbdd7f22966c221066a709c
byrongt12 <byrongt12@gmail.com>
2020-07-22 12:39:02 +0200

    Added comments

--- data-visualisation-app/src/pages/Trash/Trash.js
+++ data-visualisation-app/src/pages/Trash/Trash.js
@@ -1,5 +1,34 @@
+/**
+ *   @file Trash.js
+ *   Project: Data Visualisation Generator
+ *   Copyright: Open Source
+ *   Organisation: Doofenshmirtz Evil Incorporated
+ *
+ *   Update History:
+ *   Date        Author              Changes
+ *   -------------------------------------------------------
+ *   15/7/2020   Byron Tominson      Original
+ *
+ *   Test Cases: data-visualisation-app/src/tests/Trash.test.js
+ *
+ *   Functional Description:
+ *   Holds deleted items
+ *
+ *   Error Messages: "Error"
+ *   Assumptions: None
+ *   Constraints: None
+ */
+
+
+/**
+  * Imports
+*/
 import React from 'react';
 
+
+/**
+  * @return React Component
+*/
 function Trash() {
   return (
     <p>
