3d9e3796c8f7c842e5c907625d118145e6b7a409
byrongt12 <byrongt12@gmail.com>
2020-09-15 18:56:56 +0200

    Update Home.js

--- data-visualisation-app/src/pages/Home/Home.js
+++ data-visualisation-app/src/pages/Home/Home.js
@@ -112,7 +112,7 @@ function Home(props) {
 							<div><DashboardIllustration className='dashboardIllustration' /></div>
 							<div className='home__section--description'>
 								<div id='home__section--innerDescription'>
-									<Text id='explainDashboards'>Create a dashboard for all suggested charts or manually crafted visualizations.</Text>
+									<Text id='explainDashboards'>Login to create a dashboard for all suggested charts or manually crafted visualizations.</Text>
 								</div>
 							</div>
 						</div>
