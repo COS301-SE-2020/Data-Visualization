7fa017e307aead8a780833db5169ca1ab8c33d59
byrongt12 <byrongt12@gmail.com>
2020-09-17 00:10:41 +0200

    Fixed getFieldDecorator

--- data-visualisation-app/src/components/Entities/Entities.js
+++ data-visualisation-app/src/components/Entities/Entities.js
@@ -32,9 +32,9 @@ import Anime, { anime } from 'react-anime';
 
 var showAll = false;
 
+var entityIDs = []; 
 
 class Entities extends React.Component {
-
   
   formRef = React.createRef();
 
@@ -45,6 +45,7 @@ class Entities extends React.Component {
     data: [],
     list: [],
     allChecked: false,
+
   };
 
   onFinish = values => {
@@ -53,7 +54,7 @@ class Entities extends React.Component {
     request.user.selectedEntities = [];
     
     request.user.entitiesToDisplay.map((item) => {
-      if(this.formRef.current.getFieldValue(item.entityName) === true){
+      if(this.formRef.current.getFieldValue(item.entityName+item.datasource) === true){
         request.user.selectedEntities.push(item);
         atLeastOne = true;
       }
@@ -75,20 +76,30 @@ class Entities extends React.Component {
     
     showAll = !showAll;
     var tempItem = {};
+    console.log( entityIDs.length);
 
-    request.user.entities.map((entityName) => {
-      tempItem[entityName] = showAll;
-      this.formRef.current.setFieldsValue(tempItem);
+    entityIDs.forEach((id) => {
+      tempItem[id] = showAll;
+      //this.formRef.current.setFieldsValue(tempItem);
+
       if(showAll){
-        document.getElementById('card-'+entityName).style.backgroundColor = '#EAFFF5';
-        document.getElementById('card-'+entityName).style.boxShadow  = '0 -1.8px 1.2px #3EC195,0 1.7px 1.3px #3EC195,0 -1.5px 1px #3EC195,0 1.3px 1.9px #3EC195,0 -1.8px 3.4px #3EC195,0 1px 5px #3EC195';
+        document.getElementById('card-'+id).style.backgroundColor = '#EAFFF5';
+        document.getElementById('card-'+id).style.boxShadow  = '0 -1.8px 1.2px #3EC195,0 1.7px 1.3px #3EC195,0 -1.5px 1px #3EC195,0 1.3px 1.9px #3EC195,0 -1.8px 3.4px #3EC195,0 1px 5px #3EC195';
       }
       else{
-        document.getElementById('card-'+entityName).style.backgroundColor = '';
-        document.getElementById('card-'+entityName).style.boxShadow = '';
+        document.getElementById('card-'+id).style.backgroundColor = '';
+        document.getElementById('card-'+id).style.boxShadow = '';
       }
      
     });
+    
+    this.formRef.current.setFieldsValue(tempItem);
+    
+    // this.props.form.setFieldsValue({
+    //   [e.target.name]: e.target.value,
+    // });
+
+    
 
   }
 
@@ -102,6 +113,8 @@ class Entities extends React.Component {
     * invoked immediately after a component is mounted (inserted into the tree).
   */
   componentDidMount() {
+    entityIDs = []; 
+    
 
     request.user.selectedFields = [];
     request.user.graphTypes = ['bar','line', 'pie', 'scatter'];
@@ -147,7 +160,9 @@ class Entities extends React.Component {
             request.user.entities = Object.keys(request.user.dataSourceInfo.entityList);
             
             request.user.entities.map((entityName) => {
-    
+              console.log('here');
+              entityIDs.push(entityName+source.sourceurl);
+              
               Obj = JSON.parse(JSON.stringify(Obj));
               Obj['entityName'] = entityName;
               Obj['datasource'] = source.sourceurl;
@@ -156,6 +171,7 @@ class Entities extends React.Component {
               
               request.user.entitiesToDisplay.push(Obj);
             });
+            console.log(entityIDs.length);
             callback(request.user.entitiesToDisplay);
           }
         });
@@ -206,19 +222,21 @@ class Entities extends React.Component {
                  loadMore={loadMore}
                  dataSource = {list}
                  
-                 renderItem={item => (
-                   
+                 renderItem={item => ( 
+
                    <Fragment>
-                       <Card id = {'card-'+item.entityName} onClick={() => {                          
+                   
+                       <Card id = {'card-'+item.entityName+item.datasource} onClick={() => { 
+                                                 
                           var tempItem = {};
-                          tempItem[item.entityName] = !this.formRef.current.getFieldValue(item.entityName);
+                          tempItem[item.entityName+item.datasource] = !this.formRef.current.getFieldValue(item.entityName+item.datasource);
                           this.formRef.current.setFieldsValue(tempItem);
 
-                          if(tempItem[item.entityName]  === false){                       
-                            document.getElementById('card-'+item.entityName).style.boxShadow = '';
+                          if(tempItem[item.entityName+item.datasource]  === false){                       
+                            document.getElementById('card-'+item.entityName+item.datasource).style.boxShadow = '';
                            }
                           else{
-                            document.getElementById('card-'+item.entityName).style.boxShadow = '0px 0px 43px -12px rgba(189,189,189,1)';
+                            document.getElementById('card-'+item.entityName+item.datasource).style.boxShadow = '0px 0px 43px -12px rgba(189,189,189,1)';
                           }
                         }} 
                         >
@@ -228,7 +246,7 @@ class Entities extends React.Component {
                            actions={
                              [ 
                                
-                              <Form.Item name={item.entityName} valuePropName='checked'>
+                              <Form.Item name={item.entityName+item.datasource} valuePropName='checked'>
                                 <Checkbox/>
                               </Form.Item>
      
@@ -282,19 +300,19 @@ class Entities extends React.Component {
                  
                  renderItem={item => (
                    <Fragment>
-                       <Card.Grid hoverable = {false} className='entities__entity' id = {'card-'+item.entityName}  style= {{cursor: 'pointer', margin: '10px', marginLeft: '50px', width:'28%',  backgroundColor: 'transparent'}}  onClick={() => {
+                       <Card.Grid hoverable = {false} className='entities__entity' id = {'card-'+item.entityName+item.datasource}  style= {{cursor: 'pointer', margin: '10px', marginLeft: '50px', width:'28%',  backgroundColor: 'transparent'}}  onClick={() => {
                           var tempItem = {};
-                          tempItem[item.entityName] = !this.formRef.current.getFieldValue(item.entityName);
+                          tempItem[item.entityName+item.datasource] = !this.formRef.current.getFieldValue(item.entityName+item.datasource);
                           this.formRef.current.setFieldsValue(tempItem);
 
                 
-                          if(tempItem[item.entityName]  === false){                       
-                            document.getElementById('card-'+item.entityName).style.boxShadow  = '';
-                            document.getElementById('card-'+item.entityName).style.backgroundColor = '';
+                          if(tempItem[item.entityName+item.datasource]  === false){                       
+                            document.getElementById('card-'+item.entityName+item.datasource).style.boxShadow  = '';
+                            document.getElementById('card-'+item.entityName+item.datasource).style.backgroundColor = '';
                            }
                           else{
-                            document.getElementById('card-'+item.entityName).style.backgroundColor = '#EAFFF5';
-                            document.getElementById('card-'+item.entityName).style.boxShadow  = '0 -1.8px 1.2px #3EC195,0 1.7px 1.3px #3EC195,0 -1.5px 1px #3EC195,0 1.3px 1.9px #3EC195,0 -1.8px 3.4px #3EC195,0 1px 5px #3EC195';
+                            document.getElementById('card-'+item.entityName+item.datasource).style.backgroundColor = '#EAFFF5';
+                            document.getElementById('card-'+item.entityName+item.datasource).style.boxShadow  = '0 -1.8px 1.2px #3EC195,0 1.7px 1.3px #3EC195,0 -1.5px 1px #3EC195,0 1.3px 1.9px #3EC195,0 -1.8px 3.4px #3EC195,0 1px 5px #3EC195';
                           }
                      
                         }} 
@@ -305,7 +323,7 @@ class Entities extends React.Component {
                            actions={
                              [ 
                                
-                              <Form.Item name={item.entityName} valuePropName='checked'>
+                              <Form.Item name={item.entityName+item.datasource} valuePropName='checked'>
                                    <Checkbox style={{visibility: 'hidden'}}/>
                                </Form.Item>
      
